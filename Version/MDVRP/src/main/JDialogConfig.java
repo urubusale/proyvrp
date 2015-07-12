e86c8e14ec0814198dae287b503e08fdc7a96957
ale.garcia.cabrera <ale.garcia.cabrera@gmail.com>
2015-07-12 20:17:49 +0000

    Config.java JDialogConfig.java

---
+++ Version/MDVRP/src/main/JDialogConfig.java
@@ -0,0 +1,205 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.border.Border;
+
+import util.Config;
+
+public class JDialogConfig extends JDialog
+{
+	public JDialogConfig() 
+	{
+		super();
+		init();
+	}
+	public void init()
+	{
+		setSize(366,180);				
+	    setLocation(500,  200);
+	    setTitle("CONFIGURACIÓN");
+	    setResizable(false);
+	    
+	    final JRadioButton ningunoButton = new JRadioButton("Ninguno");
+		ningunoButton.setActionCommand("Ninguno");
+		ningunoButton.setSelected(Config.getInstancia().isNinguno());
+
+	    final JRadioButton tiempoButton = new JRadioButton("Tiempo");
+	    tiempoButton.setActionCommand("Tiempo");
+	    tiempoButton.setSelected(Config.getInstancia().isTiempo());
+	    
+	    final JRadioButton iteracButton = new JRadioButton("Iteracciones");
+	    iteracButton.setActionCommand("Iteracciones");
+	    iteracButton.setSelected(Config.getInstancia().isIteracciones());
+
+	    final JRadioButton mejoraButton = new JRadioButton("Mejora");
+	    mejoraButton.setActionCommand("Mejora");
+	    mejoraButton.setSelected(Config.getInstancia().isMejora());
+	    
+	    //Group the radio buttons.
+	    ButtonGroup group = new ButtonGroup();
+	    group.add(ningunoButton);
+	    group.add(tiempoButton);
+	    group.add(iteracButton);
+	    group.add(mejoraButton);
+	    
+		final JFormattedTextField fieldTiempo = new JFormattedTextField (new Integer(Config.getInstancia().getTiempo()));
+		final JFormattedTextField fieldIterac = new JFormattedTextField (new Integer(Config.getInstancia().getIteracciones()));
+		final JFormattedTextField fieldMejora = new JFormattedTextField (new Double(Config.getInstancia().getMejora()));
+								
+		final JLabel labelTiempo = new JLabel(" segundos");
+		final JLabel labelIterac = new JLabel(" Cantidad");
+		final JLabel labelMejora = new JLabel(" %");
+
+		if (ningunoButton.isSelected()) {
+			fieldTiempo.setEnabled(false);
+			labelTiempo.setEnabled(false);
+			fieldIterac.setEnabled(false);
+			labelIterac.setEnabled(false);
+			fieldMejora.setEnabled(false);
+			labelMejora.setEnabled(false);
+			ningunoButton.setFocusable(true);
+		}
+		if (tiempoButton.isSelected()) {
+			fieldIterac.setEnabled(false);
+			labelIterac.setEnabled(false);
+			fieldMejora.setEnabled(false);
+			labelMejora.setEnabled(false);
+			tiempoButton.setFocusable(true);
+		}
+		if (iteracButton.isSelected()) {
+			fieldTiempo.setEnabled(false);
+			labelTiempo.setEnabled(false);
+			fieldMejora.setEnabled(false);
+			labelMejora.setEnabled(false);
+			iteracButton.setFocusable(true);
+		}
+		if (mejoraButton.isSelected()) {
+			fieldTiempo.setEnabled(false);
+			labelTiempo.setEnabled(false);
+			fieldIterac.setEnabled(false);
+			labelIterac.setEnabled(false);
+			mejoraButton.setFocusable(true);
+		}
+		
+	    ningunoButton.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent arg0) {								
+				fieldTiempo.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				labelTiempo.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);								
+				fieldIterac.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				labelIterac.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				fieldMejora.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				labelMejora.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				fieldTiempo.setValue(0);
+				fieldIterac.setValue(0);
+				fieldMejora.setValue(0.0);
+	        }
+	     });
+	    
+	    tiempoButton.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent arg0) {
+				fieldIterac.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				labelIterac.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				fieldMejora.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				labelMejora.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				fieldIterac.setValue(0);
+				fieldMejora.setValue(0.0);
+	        }
+	     });
+	    
+	    iteracButton.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent arg0) {
+				fieldTiempo.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				labelTiempo.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				fieldMejora.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				labelMejora.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				fieldTiempo.setValue(0);
+				fieldMejora.setValue(0.0);
+	        }
+	     });
+	    
+	    mejoraButton.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent arg0) {
+				fieldTiempo.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				labelTiempo.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);								
+				fieldIterac.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				labelIterac.setEnabled(arg0.getStateChange() == ItemEvent.DESELECTED);
+				fieldTiempo.setValue(0);
+				fieldIterac.setValue(0);
+	        }
+	     });
+	    
+	    JPanel panel = new JPanel(new GridLayout(0, 3));
+	    
+	    Border border = BorderFactory.createTitledBorder("Condiciones de Parada");
+	    panel.setBorder(border);
+	    panel.add(ningunoButton);
+	    panel.add(new JLabel(""));
+	    panel.add(new JLabel(""));
+	    panel.add(tiempoButton);
+	    panel.add(fieldTiempo);
+	    panel.add(labelTiempo);
+	    panel.add(iteracButton);
+	    panel.add(fieldIterac);
+	    panel.add(labelIterac);
+	    panel.add(mejoraButton);
+	    panel.add(fieldMejora);
+	    panel.add(labelMejora);
+	    
+	    JPanel Jbotton=new JPanel();
+		JButton aceptar=new JButton("ACEPTAR");
+		Jbotton.add(aceptar);
+		JButton cancelar=new JButton("CANCELAR");
+		Jbotton.add(cancelar);
+		
+		
+		
+		aceptar.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{				
+				Config.getInstancia().setTiempo(tiempoButton.isSelected());
+				if (tiempoButton.isSelected())
+					Config.getInstancia().setTiempo((int) fieldTiempo.getValue());
+				
+				Config.getInstancia().setIteracciones(iteracButton.isSelected());
+				if (iteracButton.isSelected())
+					Config.getInstancia().setIteracciones((int) fieldIterac.getValue());
+				
+				Config.getInstancia().setMejora(mejoraButton.isSelected());
+				if (mejoraButton.isSelected())
+					Config.getInstancia().setMejora((double) fieldMejora.getValue());
+				
+				dispose();
+			}
+		});
+		
+		cancelar.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				dispose();
+			}
+		});
+	    JPanel p=(JPanel)this.getContentPane();
+	    p.add(panel,BorderLayout.NORTH);
+	    p.add(Jbotton,BorderLayout.SOUTH);
+			    
+		setVisible(true);
+	}
+}
---
+++ Version/MDVRP/src/util/Config.java
@@ -0,0 +1,107 @@
+package util;
+
+public class Config {
+	static private Config instancia=null;
+	static public Config getInstancia()
+	{
+		if (instancia==null)  
+		{
+			instancia= new Config();
+			return instancia;
+		}else return instancia;
+	}
+	private Config()
+	{
+		bTiempo=false;
+		bIteracciones=false;
+		bMejora=false;
+		tiempo=0;
+		iteracciones=0;
+		mejora=0;
+	}
+
+	static private boolean bTiempo;
+	static private boolean bIteracciones;
+	static private boolean bMejora;
+	static private long tiempo;
+	static private int iteracciones;
+	static private double mejora;
+	
+	static private long timeStart;
+	
+	public boolean isNinguno() {
+		return !bTiempo && !bIteracciones && !bMejora;
+	}
+	public boolean isTiempo() {
+		return bTiempo;
+	}
+	public void setTiempo(boolean bTiempo) {
+		Config.bTiempo = bTiempo;
+	}
+	public boolean isIteracciones() {
+		return bIteracciones;
+	}
+	public void setIteracciones(boolean bIteracciones) {
+		Config.bIteracciones = bIteracciones;
+	}
+	public boolean isMejora() {
+		return bMejora;
+	}
+	public void setMejora(boolean bMejora) {
+		Config.bMejora = bMejora;
+	}
+	public int getTiempo() {
+		return (int) tiempo / 1000;
+	}
+	public void setTiempo(int tiempo) {
+		Config.tiempo = tiempo * 1000;
+	}
+	public int getIteracciones() {
+		return iteracciones;
+	}
+	public void setIteracciones(int iteracciones) {
+		Config.iteracciones = iteracciones;
+	}
+	public double getMejora() {
+		return mejora;
+	}
+	public void setMejora(double mejora) {
+		Config.mejora = mejora;
+	}
+	private static void setTimeStart(long timeStart) {
+		Config.timeStart = timeStart;
+	}
+	public void empezarAlgoritmo() {		
+		setTimeStart(System.currentTimeMillis());
+	}
+	public boolean terminarPorConfig(int cantidadIteraciones, double costoAnterior, double costoMenor) {
+		long timeActual = System.currentTimeMillis();
+		
+		/*System.out.println("tiempo actual "+timeActual);
+		System.out.println("tiempo start  "+timeStart);
+		System.out.println("tiempo Config "+Config.tiempo);
+		long timesum = timeStart + Config.tiempo;
+		System.out.println("tiempo sumado "+timesum);
+		System.out.println("costoMenor "+costoMenor);
+		System.out.println("costoAnterior "+costoAnterior);
+		double costoprint = 100-((costoMenor*100)/costoAnterior);
+		System.out.println("% costo "+costoprint);
+		System.out.println("%mejora "+Config.mejora);*/
+		
+		if (Config.bIteracciones)
+			if (Config.iteracciones <= cantidadIteraciones)
+				return true;
+		
+		if (Config.bTiempo)			
+			if (timeStart + Config.tiempo <= timeActual)
+				return true;
+		
+		if (Config.bMejora)			
+			if (costoAnterior == 0)
+				return false;
+			else				
+				if ((100-(costoMenor*100)/costoAnterior) <= Config.mejora)
+					return true;		
+		return false;	
+	}
+}
